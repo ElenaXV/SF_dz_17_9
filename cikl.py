20832c0f07eb142154c3d59117e625cdceb14351
Your Name <whitefoxseven@gmail.com>
2023-02-23 01:23:44 +0300

    Initial commit

---
+++ cikl.py
@@ -0,0 +1,60 @@
+# условные выражения
+# операции сравнения
+# == равно
+# != не равно
+# > < больше или равно
+# >= <= больше или равно, меньше или равно
+
+
+# a = 5
+# b = 6
+# result = a < b or a == b  # 1 + 0 = 1
+# print(result)
+
+# оператор in
+# значение in набор значений
+
+# massage = 'hello world'
+# massage1 = 'hello'
+# print(massage1 in massage)
+
+# условная конструкция
+# if логическое выражение:
+#    инструкции
+# elif логическое выражение:
+#    инструкции
+# else:
+#    инструкции
+
+#
+#
+# language = input('введите язык ')
+# if language == 'english' or language == 'английский':
+#     print('ты учишь английский ')
+# elif language == 'русский':
+#         print('ты учишь русский ')
+# elif language == 'germany':
+#         print('ты учишь русский ')
+# else:
+#     print('ты что-то ввел не так ')
+
+# for i in range (0,11,5): # 5 это шаг
+#     print(i)
+
+# num = int(input())
+# maxs = 0
+# for i in range(num):
+#     a = int(input())
+#     if a % 5 == 0 and a > maxs:
+#         maxs = a
+# print(maxs)
+
+
+# num = int(input())
+# maxs = 0
+# while num != 0:
+#     if num % 5 == 0 and num > maxs:
+#         maxs = num
+#     num = int(input())
+# print(maxs)
+
